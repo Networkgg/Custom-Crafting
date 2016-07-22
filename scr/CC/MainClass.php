9bbf8e1286d545a8af964e7206d1b9acf74d98bf
GTNick <nickpp1111@yahoo.com>
2016-07-22 18:12:36 -0400

    Update MainClass.php

--- scr/CC/MainClass.php
+++ scr/CC/MainClass.php
@@ -9,3 +9,16 @@ use pocketmine\inventory\BigShapelessRecipe
 use pocketmine\inventory\ShapelessRecipe
 
  class ShapelessRecipe implements Recipe{
+  
+      public function onEnable(){
+        $this->getLogger()->info("CC has been Enabled!");
+      }
+
+     public function addIngredient(Item $item){
+      if(count($this->ingredients) >= 9){
+    throw new \InvalidArgumentException("Shapeless recipes cannot have more than 9 ingredients");
+
+    public function onDisable(){
+        $this->getLogger()->info("CC has been Disabled!");
+    }
+}
