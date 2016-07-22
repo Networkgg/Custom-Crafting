094585d0563a3b82c0c72d33428c49138a4a6000
GTNick <nickpp1111@yahoo.com>
2016-07-22 18:00:04 -0400

    Create MainClass.php

---
+++ scr/CC/MainClass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CC;
+
+use pocketmine\plugin\PluginBase
+use pocketmine\server
+
+use pocketmine\inventory\BigShapelessRecipe
+use pocketmine\inventory\ShapelessRecipe
+
+ class ShapelessRecipe implements Recipe{
